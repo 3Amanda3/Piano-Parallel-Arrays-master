f87038fb00b624048edb762deddfc0c23a565dd5
Amanda Schepp <mandybear1@icloud.com>
2017-04-27 14:26:42 -0500

    Piano
    use the keyboard to play piano

---
+++ Key.java
@@ -0,0 +1,67 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
+/**
+ * Key class
+ * Amanda Schepp
+ * Mr.Hardman
+ * April 20th
+ * Assignment 3
+ */
+public class Key extends Actor
+{
+    private boolean isDown = false;
+    private String key;
+    private String sound;
+    private String notPressed;
+    private String pressed;
+        
+    /**
+     * Create a new key.
+     */
+    public Key()
+    {
+    }
+    
+    /**
+     * key defines the variables and sets the image to it not being pressed
+     * @Param the parameters are String keyName and String soundFile and the String keyNotPressed and keyPressed
+     * @Return nothing is returned
+     */
+    public Key(String keyName, String soundFile, String keyNotPressed, String keyPressed)
+    {
+        key = keyName;
+        sound = soundFile;
+        notPressed = keyNotPressed;
+        pressed = keyPressed;
+        
+        setImage(notPressed + ".png");
+    }
+
+    /**
+     * Do the action for this key.
+     */
+    public void act()
+    {
+        if(isDown == false && Greenfoot.isKeyDown( key ) )
+        {          
+           setImage( pressed + ".png" );
+           isDown = true; 
+           play();
+        }        
+         if(isDown == true && !Greenfoot.isKeyDown( key ) )
+        {          
+           setImage(notPressed + ".png");
+           isDown = false; 
+        }
+    }
+    
+    /**
+     * play will play the sound of the key
+     * @Param there are no parameters
+     * @Return nothing is returned
+     */
+    public void play()
+    {
+        Greenfoot.playSound(sound + ".wav");
+    }
+}
+
